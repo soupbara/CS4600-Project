974a615976a00c7731ec9903757718269e526140
April Trinh <aprild2214@gmail.com>
2021-11-24 20:07:17 -0800

    hello world test

--- src/ClientRunner.java
+++ src/ClientRunner.java
@@ -6,7 +6,7 @@
 * Date Last Modified: 11/24/2021
 *******************************/
 public class ClientRunner{
-	public static void main(String args[0]){
-		
+	public static void main(String args[]){
+		System.out.println("hello world");
 	}
 }
