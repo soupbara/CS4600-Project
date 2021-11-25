6428a866888f3655345a3aaa997d80cebb4d463e
David Maestas <dmaestas17@gmail.com>
2021-11-24 20:02:04 -0800

    Create ClientRunner.java
    This is the ClientRunner.java file to start the client. Also testing to see if this commits.

---
+++ ClientRunner.java
@@ -0,0 +1,12 @@
+
+/*****************************
+* Purpose: This class is designed to start the client for the
+* anonymous chat program.
+*@author: David Maestas
+* Date Last Modified: 11/24/2021
+*******************************/
+public class ClientRunner{
+	public static void main(String args[0]){
+		
+	}
+}
