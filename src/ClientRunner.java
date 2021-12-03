c844a3884d474b29a54780e0ac38754c5a3eb974
David Maestas <dmaestas17@gmail.com>
2021-12-03 09:46:55 -0800

    Added more comments to ClientRunner
    Added more comments to ClientRunner to provide better documentation for the code.

--- src/ClientRunner.java
+++ src/ClientRunner.java
@@ -1,13 +1,18 @@
 
-/*****************************
+/***********************************************************
 * Purpose: This class is designed to start the client for the
-* anonymous chat program.
+* 	anonymous chat program.
 * @author: David Maestas
-* Date Last Modified: 11/24/2021
-*******************************/
+* DATE LAST MODIFIED: 12/3/2021
+************************************************************/
 public class ClientRunner{
+	
 	public static void main(String args[]){
+		//Creates an object of the ChatClient class
 		ChatClient c = new ChatClient();
+		//calls the launchWindow method from ChatClient
+		//to run the Chat Client
 		c.launchWindow();
 	}//end main
+	
 }//end ClientRunner
