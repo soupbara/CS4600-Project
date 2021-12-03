2a9a40c232ab0d520ade0b719cfd4e60ac812e70
David Maestas <dmaestas17@gmail.com>
2021-12-03 09:43:17 -0800

    Added layout for infinite loop and general comments

--- src/ServerReader.java
+++ src/ServerReader.java
@@ -1,2 +1,24 @@
+/*******************************************************************************************************
+* Purpose: This class is designed to run an infinite loop and listen in on the read byte stream
+* 	from the ChatClient class for any data sent in by the server. This class then acts in accordance to
+*	the data that it finds within the stream.
+* @author: David Maestas
+* DATE LAST MODIFIED: 12/2/2021
+******************************************************************************************************/
+
 public class ServerReader {
-}
+	
+	public static void main(String[] args){
+		
+		//boolean value that is used to create the infinite loop
+		private boolean infinite = true;
+		
+		//while loop that uses a boolean value that is always true
+		//to run an infinite loop
+		while(infinite){
+			
+		}//end while
+		
+	}//end main
+	
+}//end ServerReader
