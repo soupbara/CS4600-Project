47c3b0b9638cc04152f564683808e5757ced4133
April Trinh <aprild2214@gmail.com>
2021-12-02 20:46:16 -0800

    added skeleton receiveData() and sendData() in ChatClient.java

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -21,26 +21,44 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
+import java.io.*;
+
 public class ChatClient extends Application{
+    String key = "helloworld";
+
     /**
      * creates a byte stream over the socket connection to receive data
      * */
+    public void receiveData() throws FileNotFoundException {
+
+    }
+
     /**
      * creates a byte stream over the socket connection to send data
      */
+    public String sendData() {
+        return null;
+    }
 
     /**
      * GUI code
      */
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Chatroom");
+        primaryStage.setTitle("Chat Client");
 
-        BorderPane keyInput = new BorderPane(); //create VBox
-        Scene keyInputScene = new Scene(keyInput, 1280, 720); //create new scene
+        BorderPane keyInput = new BorderPane(); //create key input page BorderPane
+        Scene keyInputScene = new Scene(keyInput, 800, 600); //create key input scene
+
+        BorderPane chatRoom = new BorderPane();
+        Scene chatRoomScene= new Scene(chatRoom,800, 600);
 
         Label inputKeyLabel = new Label("Input Key");
         TextField inputKey = new TextField();
+        inputKey.setOnKeyReleased(e -> {
+            if (inputKey.getText().equals(key) && (e.getCode() == KeyCode.ENTER))
+                primaryStage.setScene(chatRoomScene);
+        });
         VBox inputKeyContainer = new VBox(inputKeyLabel, inputKey);
 
         keyInput.setCenter(inputKeyContainer);
