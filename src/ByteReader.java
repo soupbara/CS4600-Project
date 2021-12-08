5a5ae3eda9957f19814867c072b2ba70a90d4b27
Alexa Tang <46989357+AlexaTTang@users.noreply.github.com>
2021-12-07 17:45:43 -0800

    added stage 2 implementation in ByteReader class

--- src/ByteReader.java
+++ src/ByteReader.java
@@ -31,6 +31,8 @@ public class ByteReader
                                           //each client             1 BYTE
             byte[][] XORGroupResults = new byte[connectedClients.size()] [8];
             int numClients = connectedClients.size();
+            //two arrays to complete XOR in stage 2
+            byte[] XORresult = new byte [8];
             for(CThread client : connectedClients)
             {
                 int currentClient = connectedClients.indexOf(client);
@@ -38,7 +40,18 @@ public class ByteReader
                 byte [] clientXOR = client.getMsg(client.socket);
                 for(int i = 0; i <= 8; i++)
                 {
+                    // logging all stage one XOR results
                     XORGroupResults [currentClient][i] = clientXOR[i];
+                    //skipping XOR process for first client until we get another client stage 1 result
+                    if(connectedClients.indexOf(client) != 0)
+                    {
+                        //doing stage 2
+                        for(int k = 0; k < 8; k++)
+                        {
+                            //                       previous client stage 1 result   XOR  current client's
+                            XORresult[k] = (byte)(XORGroupResults [currentClient-1][i] ^ XORGroupResults [currentClient][i]);
+                        }
+                    }
                 }
             }
         }
